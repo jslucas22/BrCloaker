7bd2900ccee8bfa48c89e4aafd5d196e6b9775f0
José Lucas <jose.esbellute@gmail.com>
2023-05-23 19:20:26 -0300

    Tradução do index

--- admin/index.php
+++ admin/index.php
@@ -142,20 +142,13 @@ $tableOutput.="</tbody></TABLE>";
         <nav id="sidebar" class="">
             <div class="sidebar-header">
                 <a href="/admin/index.php?password=<?=$_GET['password']?><?=$date_str!==''?$date_str:''?>">
-                    <img class="main-logo" src="img/logo/logo.png" alt="" />
+                    <h4>BRCLOAKER</h4>
                 </a>
                 <strong>
                     <img src="img/favicon.png" alt="" style="width:50px" />
                 </strong>
             </div>
-            <div class="nalika-profile">
-                <div class="profile-dtl">
-                    <a href="https://t.me/yellow_web">
-                        <img src="img/notification/4.jpg" alt="" />
-                    </a>
-                    <?php include "version.php" ?>
-                </div>
-            </div>
+
             <div class="left-custom-menu-adp-wrap comment-scrollbar">
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
@@ -163,17 +156,17 @@ $tableOutput.="</tbody></TABLE>";
 
                             <a class="has-arrow" href="index.php?password=<?=$_GET['password']?><?=$date_str!==''?$date_str:''?>" aria-expanded="false">
                                 <i class="icon nalika-bar-chart icon-wrap"></i>
-                                <span class="mini-click-non">Traffic</span>
+                                <span class="mini-click-non">Tráfego</span>
                             </a>
                             <ul class="submenu-angle" aria-expanded="false">
                                 <li>
                                     <a title="Стата" href="statistics.php?password=<?=$_GET['password']?><?=$date_str!==''?$date_str:''?>">
-                                        <span class="mini-sub-pro">Statistics</span>
+                                        <span class="mini-sub-pro">Estatísticas</span>
                                     </a>
                                 </li>
                                 <li>
                                     <a title="Разрешённый" href="index.php?password=<?=$_GET['password']?><?=$date_str!==''?$date_str:''?>">
-                                        <span class="mini-sub-pro">Allowed</span>
+                                        <span class="mini-sub-pro">Permitido</span>
                                     </a>
                                 </li>
                                 <li>
@@ -183,12 +176,7 @@ $tableOutput.="</tbody></TABLE>";
                                 </li>
                                 <li>
                                     <a title="Заблокированный" href="index.php?filter=blocked&password=<?=$_GET['password']?><?=$date_str!==''?$date_str:''?>">
-                                        <span class="mini-sub-pro">Blocked</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a title="Bottom button" href="#bottom">
-                                        <span class="mini-sub-pro">Go to bottom</span>
+                                        <span class="mini-sub-pro">Bloqueado</span>
                                     </a>
                                 </li>
                             </ul>
